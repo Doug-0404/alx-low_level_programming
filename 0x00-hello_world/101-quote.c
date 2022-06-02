1fb89b8cba5a7617e5dab851c6a94c524443b5df
Doug-0404 <douglasnyakwara@gmail.com>
2022-06-02 13:37:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	const char quest[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, quest, sizeof(quest));
-	return 1;
+  write (2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",59);
+  return (1);
+
+
 }
