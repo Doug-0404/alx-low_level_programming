e02279919db4a3e5f24eb2b4afda53b28519aef1
Doug-0404 <douglasnyakwara@gmail.com>
2022-06-02 12:35:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,11 @@
-#include <stdio.h>
-
-
-
+#include<stdio.h>
 /**
+ * main - program entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: 0 always (exit main function)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
